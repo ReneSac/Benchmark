b5cc2b4536559a2f3af0466ae694bd38627a6c3b
ReneSac <reneduani@yahoo.com.br>
2014-04-30 02:30:04 -0300

    Moved tests and added an preliminary timeit()

--- benchmark.nim
+++ benchmark.nim
@@ -1,15 +1,16 @@
 # Copyright (C) 2014, René du R. Sacramento. All rights reserved.
 # MIT License. Look at license.txt for more info.
 
-import times
+import times, math, strutils
 
 type
-  TStopWatch = object
+  TStopWatch = ref object
     start_time, total_time: float
-    laps: seq[float]
-    running: bool
+    laps*: seq[float]
+    running*: bool
     
-    
+let s = TStopWatch()
+
 
 # If the program was started more than 3 hours ago in a system where float 
 # defaults to float32 we don't even have mili-seconds time precision anymore. 
@@ -24,7 +25,7 @@ proc StopWatchInit*(auto_start:bool = false): TStopWatch =
     result.start_time = cpuTime()
 
 
-proc start*(sw: var TStopWatch) {.inline} =
+proc start*(sw: var TStopWatch) {.inline.} =
   ## Starts the stop watch. Can't be called when it is already running.
   assert(not sw.running)
   sw.start_time = cpuTime()
@@ -87,52 +88,61 @@ proc isRunning*(sw: TStopWatch): bool {.inline, noSideEffect.} =
   return sw.running
 
 
-when isMainModule:
-  import math
+type TimeitResult* = tuple
+  best: float
+  loops, repeats: int
+  laps: seq[float]
   
-  proc bar(): float = 
-    var sw = StopWatchInit(auto_start = true)
-    assert sw.isRunning() == true
-    sw.lap(pause=true, store=true)
-    assert sw.isRunning() == false
-    assert sw.getLaps().len == 1
-    
-    sw.reset()
-    assert sw.peek() == 0.0
-    assert sw.getLaps().len == 0
-    
-    sw.start()
-    assert sw.isRunning() == true
-    
-    sw.lap()
-    assert sw.getLaps().len == 0
-    assert sw.isRunning() == true
-    
-    sw.pause()
-    assert sw.isRunning() == false
-    let peek = sw.peek()
-    #assert peek > 0.0  # 
-    return sw.peek()
-    
-    
+
+proc `$`*(r: TimeitResult): string =
+  result = $r.loops & " loops, best of " & $r.repeats & ": "
+  var t = r.best * 1_000_000 / r.loops.float  # time in micro seconds.
+  
+  if t < 1000:
+    result &= formatFloat(t, precision=3) & " usec per loop"
+  else:
+    t = t / 1000
+    if t < 1000:
+      result &= formatFloat(t, precision=3) & " msec per loop"
+    else:
+      result &= formatFloat(t/1000, precision=3) & " sec per loop"
+  
+template timeit*(loop: int = 0, repeat: int = 3, code: stmt): TimeitResult = 
+  var res: TimeitResult# = (loops: loop, repeats: repeat)
   var sw = StopWatchInit()
-  var sum_time: float = 0.0
-  let repeats = 100_000
+  var vloop = loop
+  var vrepeat = repeat
+  res.repeats = repeat
   
-  sw.start()
-  for i in 0 .. <repeats:
-    sum_time += bar()
-    sw.lap(store=true)
+  # If 0, automatically find a suitable loop value so that:
+  # 0.2 <= total time < 2.0 secs
+  if vloop == 0:  
+    vrepeat -= 1  # The first repeat will be the last one done here.
+    vloop = 1
     
-  let total_time = sw.pause()
-  let laps = sw.getLaps()
-  let sum_laps = laps.sum()
-  assert laps.len == repeats
-  assert total_time > sum_time
-  assert sum_laps > sum_time
-  assert total_time >= sum_laps
+    for i in 0 .. 10: # Try up to 10^10 loops. Could be a while true.
+      sw.start()
+      for i in 0 .. <vloop:
+        code
+      var x = sw.lap(store=true, pause=true)
+      if x >= 0.2:
+        break
+      else:
+        vloop *= 10
+        sw.reset()
+        
+  res.loops = vloop
+  sw.start()
+  for i in 0.. <vrepeat:
+    for j in 0.. <vloop:
+      code
+    sw.lap(store=true)
+  res.laps = sw.laps
+  res.best = min(sw.laps) # the minimum time is the most consistent.
+  res
   
-  echo("Inner partial time sum: ", sum_time)
-  echo("Laps total time: ", sum_laps, "  Laps per second: ", 60/laps.mean())
-  echo("Total time: ", total_time)
+#template timeit*(code: stmt): TimeitResult {.immediate.} = 
+#  # {.immediate.} for allowing overloading, I think.
+#  timeit(1000,3,code)
+
   
---
+++ tests/swtest.nim
@@ -0,0 +1,66 @@
+import "../benchmark"
+import math
+import os
+import times
+
+proc bar(): float = 
+  var sw = StopWatchInit(auto_start = true)
+  assert sw.isRunning() == true
+  sw.lap(pause=true, store=true)
+  assert sw.isRunning() == false
+  assert sw.getLaps().len == 1
+  
+  sw.reset()
+  assert sw.peek() == 0.0
+  assert sw.getLaps().len == 0
+  
+  sw.start()
+  assert sw.isRunning() == true
+  
+  sw.lap()
+  assert sw.getLaps().len == 0
+  assert sw.isRunning() == true
+  #sleep(100000)
+  sw.pause()
+  assert sw.isRunning() == false
+  let peek = sw.peek()
+  #assert peek > 0.0  # 
+  return sw.peek()
+    
+proc swtest() =    
+  var sw = StopWatchInit()
+  var sum_time: float = 0.0
+  let repeats = 100_000
+  
+  sw.start()
+  for i in 0 .. <repeats:
+    sum_time += bar()
+    sw.lap(store=true)
+    
+  let total_time = sw.pause()
+  let laps = sw.getLaps()
+  let sum_laps = laps.sum()
+  assert laps.len == repeats
+  assert total_time > sum_time
+  assert sum_laps > sum_time
+  assert total_time >= sum_laps
+  
+  #   for i, e in laps:
+  #     if e != 0:
+  #       echo i
+  
+  echo("Inner partial time sum: ", sum_time)
+  echo("Laps total time: ", sum_laps, "  Laps per second: ", 60/laps.mean())
+  echo("Total time: ", total_time)
+ 
+
+
+proc main() =
+  swtest()
+
+  echo timeit(100_000, 3) do:
+    discard bar()
+
+
+when isMainModule:
+  main()
